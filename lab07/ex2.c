86a023907c08db51b9bfa127ce7dabaa3d3d2511
flwfdd <1525876733@qq.com>
2023-09-03 23:19:35 +0800

    Lab 7 done

--- lab07/ex2.c
+++ lab07/ex2.c
@@ -12,10 +12,27 @@ void v_add_naive(double* x, double* y, double* z) {
 void v_add_optimized_adjacent(double* x, double* y, double* z) {
     // TODO: Implement this function
     // Do NOT use the `for` directive here!
+    #pragma omp parallel
+    {
+        int N=omp_get_num_threads();
+        for(int i=omp_get_thread_num();i<ARRAY_SIZE;i++)
+            z[i]=x[i]+y[i];
+    }
 }
 
 // Chunks Method
 void v_add_optimized_chunks(double* x, double* y, double* z) {
     // TODO: Implement this function
     // Do NOT use the `for` directive here!
+    #pragma omp parallel
+    {
+        int N=omp_get_num_threads();
+        int ind=omp_get_thread_num();
+        int size=ARRAY_SIZE/N;
+        for(int i=ind*size;i<ind*size+size;i++)
+            z[i]=x[i]+y[i];
+        if(ind==0)
+            for(int i=size*N;i<ARRAY_SIZE;i++)
+                z[i]=x[i]+y[i];
+    }
 }
